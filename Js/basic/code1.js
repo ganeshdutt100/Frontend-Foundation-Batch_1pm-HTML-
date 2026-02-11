9583c51dc60b9e6f599ef4f8fe845ddd4a0814e7
unknown <gdutt2304@gmail.com>
2026-02-11 18:17:13 +0530

    Start JS

---
+++ Js/basic/code1.js
@@ -0,0 +1,89 @@
+studentName = "Durgesh Chaudhary";
+kuchBhi = "43483984938498";
+para =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti similique quasi libero et, praesentium quod at incidunt facere hic recusandae consequatur! Deleniti exercitationem a nisi quasi perferendis excepturi ad repellendus! ";
+
+// console.log(studentName);
+// console.log(kuchBhi);
+// console.log(para);
+
+// 3 keyWord
+
+// let - ECMAScript
+// const -  ECMAScript
+// var - js
+
+// var value = "Ganesh Dutt";
+//  200 line
+// var value = "Student Name";
+// console.log(value);
+
+// let a = 10;
+// let a1 = 20;
+// console.log(a + a1);
+
+// let If = 20;
+// (a, A);
+
+// var a = 10;
+// var a = 60; -
+// a = "update "; -
+// console.log(a);
+
+// let a = 20;
+// // let a = 70;
+// a = 60;
+// console.log(a);
+
+// const b = 5;
+//  b = 78;
+// console.log(b);
+
+// N - Number; - Int , Float
+// null;
+
+// S - String;
+// Symbol;
+
+// B - BigInt;
+// Boolean;
+
+// undefined;
+
+// Number;
+// String;
+// Boolean;
+// undefined;
+// null;
+// BigInt;
+// Symbol;
+
+// let num = 43784783748374848737483748n;
+// console.log(num);
+
+// let floatNum = 434.45;
+// console.log(floatNum);
+
+// let True = true;
+// console.log(True);
+
+// Object
+// studentData
+// let name = "DC";
+// let course = "FS";
+// let Address = "B";
+// let age = 21;
+
+// let studentDate = {
+//   name: "DC",
+//   course: "FS",
+//   Address: "bulandshahr",
+//   age: 21,
+// };
+// console.log(studentDate.name);
+// console.log(studentDate.course);
+
+// let studentNames = ["Durgesh Chaudhary", "Ritul"];
+// let name1 = "Durgesh Chaudhary";
+// let name2 = "Ritul";
+// console.log(studentNames);
