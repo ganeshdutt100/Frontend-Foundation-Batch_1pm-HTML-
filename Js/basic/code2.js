1782f3d12c2773470493eb825a25ec7766b01453
unknown <gdutt2304@gmail.com>
2026-02-16 17:51:19 +0530

    Operators

---
+++ Js/basic/code2.js
@@ -0,0 +1,71 @@
+// console.log("Hello world , i am console ");
+// document.writeln("Hello world");
+// let a = 10;
+// let b = 3;
+// console.log(a % b);
+
+// print 5
+// a++
+// console.log(a++); //5
+// console.log
+
+// console.log(++a);
+
+//
+//a++; // 6
+//a--; // 5
+// console.log(a++);
+// let a = 100;
+// let b = 10;
+// let c = a / b;
+// console.log(c);
+
+// let b = 10;
+
+// let a = 10;
+// let b = "10";
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a == b);
+// console.log(a === b);
+// let True = true;
+
+// console.log(!True);
+
+// let js_is_Easy = true;
+// let js_is_Funny = false;
+
+// console.log(!js_is_Easy);
+
+// and , or , not
+// &&   , || , !
+
+// And
+// true |false =  false
+// false | true  = false
+// false | false =  false
+// true | true =  true
+
+// OR
+// true | false  = true
+// false | true  = true
+// true  | true  = true
+// false | false  = false
+
+// Not
+// !true = false
+// !false = true
+
+let a = 5;
+a = a += 10; //a = a + 10;
+a = a *= 10; //a = a * 10;
+a = a /= 10; //a = a / 10;
+a = a -= 10; //a = a - 10;
+console.log(a);
+
+let value = 18;
+let result = value % 2 == 0 ? "even" : "odd";
+console.log(result);
+
+// syntax - condition ? true : false;
