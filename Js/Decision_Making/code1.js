d056656e23463ea1632fe255594990448ee5e8c1
unknown <gdutt2304@gmail.com>
2026-02-16 18:15:10 +0530

    else if

---
+++ Js/Decision_Making/code1.js
@@ -0,0 +1,39 @@
+let a = 5;
+// syntax
+// if(condition){
+//stm
+// }
+// example->
+// if (a % 2 == 0) {
+//   console.log(a, "Even");
+// }
+//  if-else
+
+// syntax
+// if(condition){
+// true  stm
+// }else {
+// false stm
+// }
+
+// if (a % 2 == 0) {
+//   console.log(a, " is Even");
+// } else {
+//   console.log(a, " is Odd");
+// }
+
+// marks
+// 60 - first;
+// 50 - second;
+// 40 - third;
+
+// let marks = 69;
+// if (marks >= 60) {
+//   console.log("first");
+// } else if (marks >= 50) {
+//   console.log("Second");
+// } else if (marks >= 40) {
+//   console.log("Third");
+// } else {
+//   console.log("fail");
+// }
