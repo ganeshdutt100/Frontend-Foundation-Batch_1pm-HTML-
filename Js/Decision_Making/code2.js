14b8699f902fe41ea7908424719731797033198e
unknown <gdutt2304@gmail.com>
2026-02-17 18:32:15 +0530

    Nested , switch

---
+++ Js/Decision_Making/code2.js
@@ -0,0 +1,70 @@
+console.log("Code2");
+//nested if else
+// syntax
+// if (condition) {
+// stm
+//   if (condition) {
+// stm
+//   } else {
+//stm
+//   }
+// } else {
+//stm
+// }
+
+// let isConnectedToInternet = true;
+// let hasYoutubePremium = false;
+
+// if (isConnectedToInternet == true) {
+//   console.log("Connected to the internet, youtube loading...");
+//   if (hasYoutubePremium == true) {
+//     console.log("Welcome to youtube premium! Enjoy  ad-free");
+//   } else {
+//     console.log("welcome to youtube,  playing videos with ads");
+//   }
+// } else {
+//   console.log("No internet ");
+// }
+
+// syntax
+// switch (expression) {
+// case:
+// stm
+// break;
+// case:
+// stm
+// break;
+// case:
+// stm
+// break;
+// default:
+// stm
+
+// }
+day = 11;
+
+switch (day) {
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("tuesday");
+    break;
+  case 3:
+    console.log("wednesday");
+    break;
+  case 4:
+    console.log("thursday");
+    break;
+  case 5:
+    console.log("friday");
+    break;
+  case 6:
+    console.log("saturday");
+    break;
+  case 7:
+    console.log("sunday");
+    break;
+  default:
+    console.log("Invalid days");
+}
