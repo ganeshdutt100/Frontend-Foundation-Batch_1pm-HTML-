d7dde7b7372a76c597099ceeb8d5b574b831ad84
unknown <gdutt2304@gmail.com>
2026-02-18 18:42:46 +0530

    Questions

--- Js/Decision_Making/code3.js
+++ Js/Decision_Making/code3.js
@@ -25,3 +25,77 @@ console.log("Code-3");
 //   console.log(i);
 //   i++;
 // } while (i < 5);
+
+// Questions
+// 1- Print numbers from 1 to 10 using a for loop.
+
+// 2 -Print numbers from 10 to 1 (reverse order).
+
+// 3 -Print all even numbers from 1 to 50.
+
+// 4 -Print the sum of numbers from 1 to 100.
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   //   console.log(i);
+//   //   sum = sum + i;
+//   sum += i;
+//   console.log(sum);
+// }
+
+// 5-Print the multiplication table of 5.
+// let table = Number(prompt("Enter a number to print its multiplication table:"));
+// for (let i = 1; i <= 10; i++) {
+//   console.log(table, "*", i, "=", i * table);
+//   //   document.writeln(table, " * ", i, " = ", i * table, "<br>");
+// }
+
+// 2 * 1 = 2
+// 2 * 2 = 4
+// 2 * 3 = 6
+// 2 * 4 = 8
+// 2 * 5 = 10
+// 2 * 6 = 12
+// 2 * 7 = 14
+// 2 * 8 = 16
+// 2 * 9 = 18
+// 2 * 10 = 20
+
+// let a = Number(prompt("Enter your first number "));
+// let b = Number(prompt("Enter your second number "));
+// console.log(a + b);
+
+// --------------------------------------------
+// -----------------------------------
+// Print all odd numbers between 1 to 100.
+
+// Find the factorial of a number (e.g., 5 → 120).
+
+// Count how many numbers between 1 to 100 are divisible by 3.
+
+// H.W
+// Check whether a number is Prime or Not.
+
+// Print all prime numbers between 1 to 100.
+
+// Print Fibonacci series up to N terms using for loop.
+
+// Find the largest digit in a number.
+// Example: 58391 → 9
+
+// Basic Quesitons
+// Print numbers from 1 to 20.
+
+// Print numbers from 50 to 100.
+
+// Print all numbers from 1 to N (user input).
+
+// Print the square of numbers from 1 to 10.
+// Example: 1 4 9 16 ...
+
+// Print the cube of numbers from 1 to 10.
+
+// Print all multiples of 5 between 1 to 100.
+
+// Print numbers divisible by both 3 and 5 from 1 to 100.
+
+// Count total numbers from 1 to 100 using a loop.
