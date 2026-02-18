0eac5789d240330a7d44344f7e403e6adc5fcfc0
unknown <gdutt2304@gmail.com>
2026-02-18 17:43:33 +0530

    while and do while loop

--- Js/Decision_Making/code3.js
+++ Js/Decision_Making/code3.js
@@ -13,3 +13,15 @@ console.log("Code-3");
 // for (let i = 100; i >= 0; i--) {
 //   console.log(i);
 // }
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 10;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 5);
