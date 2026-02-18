42228f7a939eb887dae7458db8933af5bca36fe2
unknown <gdutt2304@gmail.com>
2026-02-18 17:35:37 +0530

    for loop

---
+++ Js/Decision_Making/code3.js
@@ -0,0 +1,15 @@
+console.log("Code-3");
+// console.log(1, " Avi");
+// console.log(2, " Avi");
+// console.log(3, " Avi");
+// console.log(4, " Avi");
+// console.log(5, " Avi");
+// console.log(6, " Avi");
+
+// for (let i = 0; i <= 100; i++) {
+//   console.log(i, "Ganesh Dutt");
+// }
+
+// for (let i = 100; i >= 0; i--) {
+//   console.log(i);
+// }
