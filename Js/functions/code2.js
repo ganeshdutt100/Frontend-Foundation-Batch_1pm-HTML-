907c843a7b907b5404bba4ed128ca1d71b81016e
unknown <gdutt2304@gmail.com>
2026-02-25 17:36:34 +0530

    functions

---
+++ Js/functions/code2.js
@@ -0,0 +1,41 @@
+// function fun() {
+//   console.log("hello  world");
+// }
+
+// fun();
+// function greet(name) {
+//   return "Ganesh";
+// }
+// console.log(greet("Dutt"));
+
+// const add = function (a, b) {
+//   return a + b;
+// };
+// console.log(add(23, 98));
+// const multiply = (a, b) => a * b;
+// console.log(multiply(12, 89));
+
+// (() => console.log("hello"))();
+
+// const supplementsDiscount = (price, discount) => {
+//   let discountAmount = (price * discount) / 100;
+//   let finalPrice = price - discountAmount;
+//   return console.log(
+//     "final price after ",
+//     discountAmount,
+//     " % discount ",
+//     finalPrice,
+//   );
+// };
+
+// supplementsDiscount(100, 20);
+
+function checkAuth(isLoggedIn) {
+  if (isLoggedIn) {
+    return "Welcome to the Dashboard";
+  } else {
+    return "please signIn first";
+  }
+}
+let useLogged = true;
+console.log(checkAuth(useLogged));
