724f93809de641e788d74145b43d80537d225faa
unknown <gdutt2304@gmail.com>
2026-03-12 17:27:00 +0530

    game

---
+++ Js/array/arr2.js
@@ -0,0 +1,24 @@
+let arr = [1, 2, 3, 45, 43, 2, 34, 2];
+
+// let output = arr.join("-");
+// console.log(output);
+// console.log(arr);
+
+// let arr1 = [...new Set(arr)];
+// console.log(arr1);
+
+// let value = [];
+// for (let x of arr) {
+//   if (!value.includes(x)) {
+//     value.push(x);
+//   }
+// }
+// console.log(value);
+
+// let gameValues = ["rock", "paper", "scissors"];
+
+// let computer = gameValues[Math.floor(Math.random() * 3)];
+
+// console.log(computer);
+
+// let user  = prompt()
--- Js/array/arrMethods.js
+++ Js/array/arrMethods.js
@@ -7,5 +7,24 @@ let arr = [1, 9, 12, 2, 3, 4, 5, 8];
 // arr.reverse();
 // arr.sort();
 // arr.sort((a, b) => a - b);
-arr.splice(1, 3, "JS", "num");
-console.log(arr);
+// arr.splice(1, 3, "JS", "num");
+// console.log(arr);
+
+//Array e index 2 se  elements remove karo aur unki jagah "JS" and "React" add karo
+// Input:
+// let arr = [10, 20, 30, 40, 50]
+
+//2- Add "HTML" at the beginning of the array.
+// [20,30,40]
+
+// 3-Remove the last element from the array.
+// let arr = [10, 20, 30, 40, 50]
+
+// 4-Fill "JS" from index 1 to index 3.
+// ["HTML","CSS","React","Node"]
+
+// 5- Insert "MongoDB" at index 2.
+// ["HTML","CSS","JS"]
+
+// 6-Replace "CSS" with "Tailwind".
+// ["HTML","CSS","JS"]
